db459ab1f46d344f6266b5894191dd09a8445734
Nihat <abdullazadenihat@gmail.com>
2023-04-30 15:47:05 +0400

    added test case

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,10 +1,14 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] < arr[j]) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
         [arr[i], arr[j]] = [arr[j], arr[i]]
       }
     }
   }
   return arr
 }
+
+let testArr = [10, 50, 20, 30, 1, 88, 23, 67, 1025, 2, 7]
+
+console.log(bubbleSort(testArr))
